2f843d04defd798b484464e04c903d994376270f
BakkaBoi69 <98868015+BakkaBoi69@users.noreply.github.com>
2023-07-31 13:01:28 +0530

    Add mouseEnterGrid() functionality for Color Mode and Eraser Mode

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const gridWrapper = document.querySelector('.grid-wrapper');
-const clearButton = document.querySelector('#clear-button');
-const gridSlider = document.getElementById('slider');
-const colorWheel = document.getElementById('colorpicker');
-const sliderLabel = document.getElementById('slider-label');
-let mouseDown = false;
-let mouseUp = true;
-let colorMode = true;
-let randomMode = false;
-let color = colorWheel.value;
-let gridCells = 16;
+const gridWrapper = document.querySelector('.grid-wrapper'); // Wrapper for the grid cells
+const clearButton = document.querySelector('#clear-button'); // Button used to return all colors to default value
+const gridSlider = document.getElementById('slider'); // Slider which determines size of grid
+const colorWheel = document.getElementById('colorpicker'); // Color picker for different colors
+const sliderLabel = document.getElementById('slider-label'); // Slider label that displays the current slider value
+const colorButton = document.getElementById('color-mode-button'); // Button to select color mode
+const eraser = document.getElementById('eraser-button'); // button to select eraser mode
+const randomButton = document.getElementById('random-mode-button'); // button to select random mode
+let mouseDown = false; // Left-click is pressed
+let mouseUp = true; // Left-click is not pressed
+let drawMode = 'color'; // different drawing modes
+let color = colorWheel.value; // Currently selected color from color wheel
+let gridCells = 16; // Number of grid cells to divide the grid into
+
+// Set event listeners for all different modes of drawing
+colorButton.addEventListener('click', () => drawMode = 'color');
+randomButton.addEventListener('click', () => drawMode = 'random');
+eraser.addEventListener('click', () => drawMode = 'erase');
 
 // Event listener for the slider to change grid size
 gridSlider.addEventListener('input', () => {
@@ -26,28 +33,51 @@ colorWheel.addEventListener('input', () => {
     color = colorWheel.value;
 });
 
+// Event listener is added for document because
+// I should be able to click outside the grid and 
+// move into the grid and it should still work
+
+// Detects whether mouse is moving
 document.addEventListener('mousemove', mouseEnterGrid);
+// Detects whether mouse is down
 document.addEventListener('mousedown', handleMouseDown);
+// Detects whether mouse is up
 document.addEventListener('mouseup', handleMouseUp);
 
+// Handles the logic of Mouse up
 function handleMouseUp() {
     mouseUp = true;
     mouseDown = false;
 }
 
+// Handles the logic of Mouse down
 function handleMouseDown() {
     mouseDown = true;
     mouseUp = false;
 }
 
+// This function ensures that the mouse can be moved into
+// the grid from outside and it should still color
 function mouseEnterGrid(e) {
     if (mouseDown) {
-        e.target.style.backgroundColor = color;
+        if (e.target.classList.contains('grid-cell')) {
+            if (drawMode == 'color') {
+                e.target.style.backgroundColor = color;
+            } else if (drawMode == 'erase') {
+                e.target.style.backgroundColor = 'rgb(28, 28, 28)';
+            }
+        } 
     }
 }
 
 // Function to create grid dynamically
 function createGrid() {
+    // These 2 lines use "grid" mode in CSS which is specifically designed
+    // to handle these type of 2 dimensional grids
+
+    // `repeat(${gridCells}, 1fr)` 
+    // Generates a CSS Grid `repeat` function string that will create a grid
+    // with `gridCells` number of equally sized columns or rows. 
     gridWrapper.style.gridTemplateColumns = `repeat(${gridCells}, 1fr)`;
     gridWrapper.style.gridTemplateRows = `repeat(${gridCells}, 1fr)`;
     for (let i = 0; i < gridCells*gridCells; i++) {
@@ -56,12 +86,7 @@ function createGrid() {
         cell.style.width = (gridWrapper.width / gridCells);
         cell.style.height = cell.style.width;
         gridWrapper.appendChild(cell);
-        cell.addEventListener('mouseover', changeColor);
     }
-}
-
-function changeColor(e) {
-    e.target.style.backgroundColor = color;
 }
 
 function clearGrid() {
